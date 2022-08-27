a21170f9902502925c43187864b9d21964d5e053
Siddharth263 <priyanshulal2213@gmail.com>
2022-08-27 17:59:08 +0530

    updates

--- All JS/cart.js
+++ All JS/cart.js
@@ -7,34 +7,7 @@ document.querySelector("#footer").innerHTML = footer();
 
 
 
-let user = [
-  {
-    id: 1,
-    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    price: 109.95,
-    description:
-      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-    category: "men's clothing",
-    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    rating: {
-      rate: 3.9,
-      count: 120,
-    },
-  },
-  {
-    id: 1,
-    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    price: 1000,
-    description:
-      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-    category: "men's clothing",
-    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    rating: {
-      rate: 3.9,
-      count: 120,
-    },
-  },
-];
+let user = JSON.parse(localStorage.getItem("PRODUCTCART")) || [];
 let totalprice = 0;
 
 let x = user.length;
