8bb3a102838679778edf0f90bb254c74f327f48b
Siddharth263 <priyanshulal2213@gmail.com>
2022-08-27 17:22:58 +0530

    brandNavbar updated

--- ALL JS/index.js
+++ ALL JS/index.js
@@ -1,7 +1,6 @@
 import foot from '../components/footer.js';
 
+import brandNavbar from '../components/brandNavbar.js'
+
 let footer = document.querySelector("#footer");
 footer.innerHTML = foot();
-
-// api : https://fakestoreapi.com/products/
-
